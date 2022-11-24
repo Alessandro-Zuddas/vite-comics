c835f12ae6005b32637f2a0a80b794fcc882f21d
Alessandro-Zuddas <alessandro.zuddas1999@gmail.com>
2022-11-24 17:09:00 +0100

    Aggiunge AppMain

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -2,7 +2,32 @@
 export default {
   data() {
     return {
-      
+        
+        objs: [
+
+            {
+                name: "DIGITAL COMICS",
+                icon: "src/assets/img/buy-comics-digital-comics.png"
+            },
+            {
+                name: "DC MERCHANDISE",
+                icon: "src/assets/img/buy-comics-merchandise.png"
+            },
+            {
+                name: "SUBSCRIPTION",
+                icon: "src/assets/img/buy-comics-subscriptions.png"
+            },
+            {
+                name: "COMIC SHOP LOCATION",
+                icon: "src/assets/img/buy-comics-shop-locator.png"
+            },
+            {
+                name: "DC POWER VISA",
+                icon: "src/assets/img/buy-dc-power-visa.svg"
+            },
+
+        ]
+
     }
   }
 }
@@ -10,31 +35,43 @@ export default {
 
 <template>
   
-  <div class="container-md">
-    <div>
-        Prova
+    <div class="row">
+        <div class="container-md">
+            <div v-for="(obj, index) in objs">
+                <img :src="`${ obj.icon }`" alt="Icon">
+                <strong>{{ obj.name }}</strong>
+            </div>
+        </div>
     </div>
-    <div>
-        Prova
-    </div>
-    <div>
-        Prova
-    </div>
-    <div>
-        Prova
-    </div>
-    <div>
-        Prova
-    </div>
-  </div>
 
 </template>
 
 <style lang="scss" scoped>
 
+.row{
+    background-color: var(--first-color);
+}
+
 .container-md{
     display: flex;
     justify-content: space-between;
+}
+
+div{
+    display: flex;
+    align-items: center;
+    padding-top: var(--padd-xl);
+    padding-bottom: var(--padd-xl);
+}
+
+img{
+    width: 4.6875rem;
+    padding-right: var(--padd-sm);
+}
+
+strong{
+    font-size: .9375rem;
+    padding-right: var(--padd-xxl);
 }
 
 </style>
